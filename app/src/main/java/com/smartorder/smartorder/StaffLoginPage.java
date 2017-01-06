844b881795f5375a9e27abda4a2ff7b54c92586d
UmarZaii <umarzaii96@gmail.com>
2017-01-07 01:57:20 +0800

    hai2

--- app/src/main/java/com/smartorder/smartorder/StaffLoginPage.java
+++ app/src/main/java/com/smartorder/smartorder/StaffLoginPage.java
@@ -6,10 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * Created by mansoull on 6/1/2017.
- */
-
 public class StaffLoginPage extends AppCompatActivity implements View.OnClickListener {
 
     Button logStaff;
