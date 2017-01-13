44c0dd36eaa74fe8ce7d9822e1514e90fda7d7c8
UmarZaii <umarzaii96@gmail.com>
2017-01-14 00:18:53 +0800

    hai2

--- app/src/main/java/com/smartorder/smartorder/AdminMenuList.java
+++ app/src/main/java/com/smartorder/smartorder/AdminMenuList.java
@@ -1,45 +1,80 @@
 package com.smartorder.smartorder;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.Toast;
+import android.util.Log;
 
 
-public class AdminMenuList extends AppCompatActivity {
+public class AdminMenuList extends AppCompatActivity implements OnItemClickListener {
 
-    Spinner spnMenuType;
-    ArrayAdapter<CharSequence> adpMenuType;
+//    Spinner spnMenuType;
+//    ArrayAdapter<CharSequence> adpMenuType;
+    String[] dataMenuList;
+    ListView menuList = BackgroundTask.menuList;
+    protected Cursor cursor;
+    DBHandler dbhandler;
+    public static String selection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menulist);
+        menuList = (ListView)findViewById(R.id.dispMenuList);
+        menuList.setOnItemClickListener(AdminMenuList.this);
+        BackgroundTask bgTask = new BackgroundTask(this);
+        bgTask.execute("disp_menu");
 
         //ComboBox Menu Type
-        spnMenuType = (Spinner) findViewById(R.id.spnMenuType);
-        adpMenuType = ArrayAdapter.createFromResource(this,R.array.menu_types,android.R.layout.simple_spinner_item);
-        adpMenuType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spnMenuType.setAdapter(adpMenuType);
-        spnMenuType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(AdminMenuList.this, parent.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
-            }
+//        spnMenuType = (Spinner) findViewById(R.id.spnMenuType);
+//        adpMenuType = ArrayAdapter.createFromResource(this,R.array.menu_types,android.R.layout.simple_spinner_item);
+//        adpMenuType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spnMenuType.setAdapter(adpMenuType);
+//        spnMenuType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(AdminMenuList.this, parent.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+    }
 
-            }
-        });
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        dbhandler = new DBHandler(this, null, null, 1);
+        SQLiteDatabase db = dbhandler.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM ordermenu",null);
+        dataMenuList = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc=0; cc < cursor.getCount(); cc++){
+            cursor.moveToPosition(cc);
+            dataMenuList[cc] = cursor.getString(2).toString();
+        }
+        final String selection = dataMenuList[position];
+        this.selection = selection;
+        Log.d("Selection", selection);
+
+        startActivity(new Intent(this, AdminViewMenuDetails.class));
 
     }
 
@@ -47,8 +82,8 @@ public class AdminMenuList extends AppCompatActivity {
         startActivity(new Intent(this, AdminMenuAdd.class));
     }
 
-    public void viewMenuPage(View view) {
-        startActivity(new Intent(this, DisplayMenuList.class));
-    }
+//    public void viewMenuPage(View view) {
+//        startActivity(new Intent(this, DisplayMenuList.class));
+//    }
 
 }
--- app/src/main/java/com/smartorder/smartorder/AdminViewMenuDetails.java
+++ app/src/main/java/com/smartorder/smartorder/AdminViewMenuDetails.java
@@ -27,7 +27,7 @@ public class AdminViewMenuDetails extends AppCompatActivity {
         lblMenuStatusDB = (TextView) findViewById(R.id.lblMenuStatusDB);
 
         SQLiteDatabase db = dbHandler.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM ordermenu WHERE menuname = '" + DisplayMenuList.selection + "'",null);
+        Cursor cursor = db.rawQuery("SELECT * FROM ordermenu WHERE menuname = '" + AdminMenuList.selection + "'",null);
         while(cursor.moveToNext()) {
             strMenuNameDB = cursor.getString(cursor.getColumnIndex(DBHandler.COLUMN_MENUNAME));
             strMenuPriceDB = cursor.getString(cursor.getColumnIndex(DBHandler.COLUMN_MENUPRICE));
