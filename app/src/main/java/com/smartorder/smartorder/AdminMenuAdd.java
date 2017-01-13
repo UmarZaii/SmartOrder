606b02c1b3b364758aac66bf43a99d217535a442
UmarZaii <umarzaii96@gmail.com>
2017-01-14 01:23:27 +0800

    hai2

--- app/src/main/java/com/smartorder/smartorder/AdminMenuAdd.java
+++ app/src/main/java/com/smartorder/smartorder/AdminMenuAdd.java
@@ -12,7 +12,6 @@ public class AdminMenuAdd extends AppCompatActivity {
     EditText txtMenuName, txtMenuPrice, txtMenuStatus;
     DBHandler dbHandler;
 
-    //testcommite
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
